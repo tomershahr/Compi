f1f27f4723883b48624981781e9f60c5c05a124f
tomershahr <49405633+tomershahr@users.noreply.github.com>
2021-05-22 11:32:41 +0300

    Add files via upload

---
+++ tables.cpp
@@ -0,0 +1,18 @@
+
+
+using namespace std;
+
+struct Row{
+	
+	string name;
+	strin type;
+	int offset;
+};
+
+
+bool insertRow(vector<rows> table, char * name, char * type, int offset){
+		for (int i -0 ; i<table.size();i++){
+		
+		}
+	
+}
